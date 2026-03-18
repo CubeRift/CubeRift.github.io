69a39bce197afbe5929a333d71a38b4e45b8d0ed
CubeRift <cuberift@gmail.com>
2026-03-18 14:21:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // CodeVault Configuration
 // Replace the token below with your GitHub Personal Access Token (gist scope)
-const CONFIG_TOKEN = 'YOUR_TOKEN_HERE';
+const CONFIG_TOKEN = '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';
